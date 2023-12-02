8ada46e0659ea00a6a2b635b5b3777eb7ba2bc78
faraclara <faraadevianaa@gmail.com>
2023-12-02 15:29:21 +0800

    frontend login register

---
+++ app/src/main/java/com/example/yougoapp/SignupTabFragment.kt
@@ -0,0 +1,18 @@
+package com.example.yougoapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import com.example.yougoapp.databinding.FragmentSignupTabBinding
+
+
+class SignupTabFragment: Fragment(R.layout.fragment_signup_tab) {
+
+    private var _binding: FragmentSignupTabBinding? = null
+    private val binding get() = _binding!!
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentSignupTabBinding.bind(view)
+
+    }
+}
