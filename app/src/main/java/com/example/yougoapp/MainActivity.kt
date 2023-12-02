d84324bae2f66d3cf687d32384c3e9fa41734f8e
faraclara <faraadevianaa@gmail.com>
2023-12-02 16:03:23 +0800

    login

--- app/src/main/java/com/example/yougoapp/MainActivity.kt
+++ app/src/main/java/com/example/yougoapp/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.yougoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.yougoapp.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bundle = Bundle()
+
+        val sectionPagerAdapter = SectionPagerAdapter(this, supportFragmentManager, bundle)
+        binding.apply {
+            viewPager.adapter = sectionPagerAdapter
+            this.tabs.setupWithViewPager(viewPager)
+        }
     }
 }
