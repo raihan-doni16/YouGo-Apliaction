7be067756cd244ef4b4d6903a3139bd9c1005202
faraclara <faraadevianaa@gmail.com>
2023-12-03 16:46:57 +0800

    Profile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id ("kotlin-kapt")
 }
 
 android {
@@ -47,4 +48,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+    kapt ("com.github.bumptech.glide:compiler:4.12.0")
+
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
 }
